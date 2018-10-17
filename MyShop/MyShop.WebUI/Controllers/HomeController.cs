65c766115f5c375fbe9b01999c724484397664f0
salocinmello <42628339+salocinmello@users.noreply.github.com>
2018-10-17 00:11:53 -0300

    Criação de página inicial com lista de produtos e página de visualização de detalhes

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,33 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategory;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategory = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
         }
 
-        public ActionResult About()
+        public ActionResult Details(string Id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }        
     }
 }
