c833bcc64735a86268b878550a30e04e9f301a7f
salocinmello <42628339+salocinmello@users.noreply.github.com>
2018-12-12 23:25:48 -0200

    Inicial de UnitTest

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Model;
+using MyShop.Core.ViewModel;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
@@ -13,8 +16,19 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+
+            productContext.Insert(new Product());
+
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
+            HomeController homeController = new HomeController(productContext, productCategoryContext);
+
+            var result = homeController.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            Assert.IsTrue(2 == viewModel.Products.Count());
             //// Arrange
             //HomeController controller = new HomeController();
 
